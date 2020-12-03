65d78fd53528f81ef68eb0915b7e4b385563ca4f
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 14:45:21 +0100

    removed things from other branches

--- src/main/java/BukkitPlugin.java
+++ src/main/java/BukkitPlugin.java
@@ -1,57 +1,6 @@
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.logging.*;
-
 public class BukkitPlugin extends JavaPlugin {
-
-    @Override
-    public void onLoad() {
-        super.onLoad();
-
-        // Create a directory for the plugin in the logs directory of the server
-        String logDirectory = String.format("./logs/%s", getName());
-        new File(logDirectory).mkdirs();
-
-        // Get the logger
-        Logger logger = getLogger();
-        try {
-            // Create a new file handler
-            FileHandler fileHandler = new FileHandler(String.format(
-                    "%s/%s.log",
-                    logDirectory,
-                    new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-SSS").format(new Date())
-            ));
-            // Set the format of the log messages
-            // fileHandler.setFormatter(new SimpleFormatter());
-            fileHandler.setFormatter(new Formatter() {
-                @Override
-                public String format(LogRecord record) {
-                    return String.format(
-                            "[%s][%s]: %s\n",
-                            new SimpleDateFormat("HH:mm:ss.SSS").format(new Date(record.getMillis())),
-                            record.getLevel(),
-                            record.getMessage()
-                    );
-                }
-            });
-            // Set the log level of the file handler
-            fileHandler.setLevel(Level.ALL);
-            // Finally add the handler to the logger
-            logger.addHandler(fileHandler);
-            logger.info("New File Handler added");
-        } catch (IOException e) {
-            // Log a warning, if adding the handler somehow failed
-            logger.warning("Couldn't add File Handler. Read stack trace below for more details");
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void onEnable() {
@@ -79,10 +28,4 @@ public class BukkitPlugin extends JavaPlugin {
                 return false;
         }
     }*/
-
-    @Override
-    public void onDisable() {
-        super.onDisable();
-        Arrays.stream(getLogger().getHandlers()).forEach(Handler::close);
-    }
 }
