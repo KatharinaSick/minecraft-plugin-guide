84072ac83a4a327a104609d94c8e602b219946e2
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 14:32:28 +0100

    removed onCommand of plugin class

--- src/main/java/BukkitPlugin.java
+++ src/main/java/BukkitPlugin.java
@@ -62,24 +62,6 @@ public class BukkitPlugin extends JavaPlugin {
         getCommand("log-anything").setExecutor(logCommandExecutor);
     }
 
- /*   @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        switch (command.getName().toLowerCase()) {
-            case "log":
-                if (args.length < 1) {
-                    sender.sendMessage("No message given");
-                    return false;
-                }
-                getLogger().info(String.join(" ", args));
-                return true;
-            case "log-anything":
-                getLogger().info("\"log-anything\" command was triggered. So here's your log message");
-                return true;
-            default:
-                return false;
-        }
-    }*/
-
     @Override
     public void onDisable() {
         super.onDisable();
